29a28b3f8513d2062cd64b4d034d3512b4af98a6
HeroGamers <hero@herogamers.dev>
2022-05-10 16:53:11 +0200

    Merge both options

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import sys
 import pyperclip
 
+inputminuted = False
+
 if __name__ == "__main__":
     root_path = None
     # Read path from given command-line argument
@@ -12,16 +14,17 @@ if __name__ == "__main__":
     # If no argument was given, use base directory
     if not root_path:
         root_path = os.path.dirname(os.path.realpath(__file__))
-    pathname = os.path.join(root_path, "src")
+    # pathname = os.path.join(root_path, "src")
+    pathname = root_path
 
     latex_output = "    \\subsection{Code} \label{code}\n\n"
 
     for root, dirs, files in os.walk(pathname, topdown=True):
         for filename in files:
-            if "." not in filename or filename.split(".")[1] in ["png", "svg", "puml", "gitignore", "properties"]:
-                continue
-
             filepath = os.path.join(root, filename)
+
+            if "." not in filename or filename.split(".")[1] in ["png", "svg", "puml", "gitignore", "properties"] or any(x in filepath for x in ["cmake-build-debug", ".idea"]):
+                continue
 
             # If it's a png or smthn, we can't read it, so read it first.
             file_code = ""
@@ -34,20 +37,31 @@ if __name__ == "__main__":
             minted_language = str(filename.split(".")[1])
             if minted_language == "py":
                 minted_language = "python"
+            elif minted_language == "h":
+                minted_language = "c"
+            elif minted_language == "txt":
+                minted_language = "text"
 
-            latex_output = latex_output + "        \\subsubsection{"+(filepath.replace(root_path+"\\src\\", "")).replace("\\", "/")+"} \label{"+filename.lower().replace(".", "_")+"}\n" \
-                                          "\n" \
-                                          "        \\begin{longlisting}\n" \
-                                          "            \\centering\n" \
-                                          "            \\inputminted[\n" \
-                                          "                framesep=4mm,\n" \
-                                          "                baselinestretch=1,\n" \
-                                          "                breaklines,\n" \
-                                          "                breakanywhere\n" \
-                                          "            ]{"+minted_language+"}{project_files/"+(filepath.replace(root_path+"\\src\\", "")).replace("\\", "/")+"}\n" \
-                                          "            \\caption{"+filename+"}\n" \
-                                          "            \\label{lst:"+filename.lower().replace(".", "_")+"}\n" \
-                                          "        \\end{longlisting}\n" \
-                                          "\n"
+            filepath_document = (filepath.replace(pathname, "")).replace("\\", "/")
+            if filepath_document.startswith("/"):
+                filepath_document = filepath_document[1:]
+
+            latex_output = str(
+                f"{latex_output}"
+                "        \\subsubsection{"+filepath_document+"} \label{"+filename.lower().replace(".", "_")+"}\n"
+                "\n"
+                "        \\begin{longlisting}\n"
+                "            \\centering\n" +
+                ("            \\begin{minted}[\n" if not inputminuted else "            \\inputminted[\n") +
+                "                framesep=4mm,\n"
+                "                baselinestretch=1,\n"
+                "                breaklines,\n"
+                "                breakanywhere\n"
+                "            ]{"+minted_language+"}" + (("{project_files/"+(filepath.replace(root_path+"\\src\\", "")).replace("\\", "/")+"}\n") if inputminuted else f"\n{file_code}\n            \\end{'{minted}'}\n") +
+                "            \\caption{"+filename+"}\n"
+                "            \\label{lst:"+filename.lower().replace(".", "_")+"}\n"
+                "        \\end{longlisting}\n"
+                "\n"
+                               )
     pyperclip.copy(latex_output)
     print("Output copied to clipboard!")
